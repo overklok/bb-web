aa7d60e75ddc978187ea2ff73fd10d78385dba96
borland <john_overklok@yahoo.com>
2019-09-16 17:42:44 +0500

    autodeploy test 2

--- app/js/index.js
+++ app/js/index.js
@@ -121,7 +121,7 @@ class Application {
             'bb:change', 'bb:drag-start'
         ]);
 
-        console.log("autodeploy test");
+        console.log("autodeploy test 2");
     }
 
     setAdmin(isAdmin) {
