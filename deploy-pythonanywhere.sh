04e0e88fe9115fcd9035210c95cfbcadd96aef21
borland <john_overklok@yahoo.com>
2019-09-16 19:00:33 +0500

    depl manage.py add 3servers

--- deploy-pythonanywhere.sh
+++ deploy-pythonanywhere.sh
@@ -10,15 +10,49 @@ npm install
 npm run build
 
 # For PythonAnywhere servers
+
+# srv-main
 cp /home/overklok/tapanda/ch-board/jsapp/dist/bundle.js /home/overklok/tapanda/ch-board/srv-main/frontend/static/frontend/app/js/
 cp /home/overklok/tapanda/ch-board/jsapp/dist/admin_blockly.js /home/overklok/tapanda/ch-board/srv-main/coursesvc/static/admin/vendor/admin-blockly/js/
 cp /home/overklok/tapanda/ch-board/jsapp/dist/admin_board.js /home/overklok/tapanda/ch-board/srv-main/coursesvc/static/admin/vendor/admin-board/js/
 
-workon ch-board-srv-main
+# srv-alpha1
+cp /home/overklok/tapanda/ch-board/jsapp/dist/bundle.js /home/overklok/tapanda/ch-board/srv-alpha1/frontend/static/frontend/app/js/
+cp /home/overklok/tapanda/ch-board/jsapp/dist/admin_blockly.js /home/overklok/tapanda/ch-board/srv-alpha1/coursesvc/static/admin/vendor/admin-blockly/js/
+cp /home/overklok/tapanda/ch-board/jsapp/dist/admin_board.js /home/overklok/tapanda/ch-board/srv-alpha1/coursesvc/static/admin/vendor/admin-board/js/
 
+# srv-alpha2
+cp /home/overklok/tapanda/ch-board/jsapp/dist/bundle.js /home/overklok/tapanda/ch-board/srv-alpha2/frontend/static/frontend/app/js/
+cp /home/overklok/tapanda/ch-board/jsapp/dist/admin_blockly.js /home/overklok/tapanda/ch-board/srv-alpha2/coursesvc/static/admin/vendor/admin-blockly/js/
+cp /home/overklok/tapanda/ch-board/jsapp/dist/admin_board.js /home/overklok/tapanda/ch-board/srv-alpha3/coursesvc/static/admin/vendor/admin-board/js/
+
+# srv-alpha3
+cp /home/overklok/tapanda/ch-board/jsapp/dist/bundle.js /home/overklok/tapanda/ch-board/srv-alpha3/frontend/static/frontend/app/js/
+cp /home/overklok/tapanda/ch-board/jsapp/dist/admin_blockly.js /home/overklok/tapanda/ch-board/srv-alpha3/coursesvc/static/admin/vendor/admin-blockly/js/
+cp /home/overklok/tapanda/ch-board/jsapp/dist/admin_board.js /home/overklok/tapanda/ch-board/srv-alpha3/coursesvc/static/admin/vendor/admin-board/js/
+
+# srv-main
+workon ch-board-srv-main
 python /home/overklok/tapanda/ch-board/srv-main/manage.py collectstatic --no-input
 touch /var/www/board_tapanda_ru_wsgi.py
+deactivate
 
+# srv-alpha1
+workon ch-board-srv-alpha1
+python /home/overklok/tapanda/ch-board/srv-alpha1/manage.py collectstatic --no-input
+touch /var/www/alpha1_tapanda_ru_wsgi.py
+deactivate
+
+# srv-alpha2
+workon ch-board-srv-alpha2
+python /home/overklok/tapanda/ch-board/srv-alpha2/manage.py collectstatic --no-input
+touch /var/www/alpha2_tapanda_ru_wsgi.py
+deactivate
+
+# srv-alpha3
+workon ch-board-srv-alpha3
+python /home/overklok/tapanda/ch-board/srv-alpha3/manage.py collectstatic --no-input
+touch /var/www/alpha3_tapanda_ru_wsgi.py
 deactivate
 
 # hooks/post-receive (+x):
