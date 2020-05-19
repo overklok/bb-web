85b02139637b8d3aadd4765ef0c1c61903fd9c5f
overklok-pc <john_overklok@yahoo.com>
2020-05-19 14:17:03 +0500

    motor plate image

--- app/js/utils/breadboard/plates/MotorPlate.js
+++ app/js/utils/breadboard/plates/MotorPlate.js
@@ -17,10 +17,8 @@ export default class MotorPlate extends LinearPlate {
      * @param {string}  orientation     ориентация RGB-диода
      */
     __draw__(position, orientation) {
-        this._bezel.fill({color: "#f0a67a", opacity: 1});
-
         this._drawPicture();
-        this._drawLabel('мотор', 40);
+        // this._drawLabel('мотор', 40);
 
         // this._group.text(`Diode ${this._params.colour}`).font({size: 20});
     };
@@ -30,10 +28,57 @@ export default class MotorPlate extends LinearPlate {
      * @param {number} qs размер квадрата
      * @private
      */
-    _drawPicture(qs=Plate.LabelSizeDefault) {
+    _drawPicture(qs=Plate.QuadSizeDefault) {
         let cell1 = this.__grid.cell(0, 0);
+        let cell2 = this.__grid.cell(this._params.size.x-1, this._params.size.y-1);
+
         let rect1 = this._group.rect(qs, qs);
+        let rect2 = this._group.rect(qs, qs);
+
         rect1.center(cell1.center_rel.x, cell1.center_rel.y);
+        rect2.center(cell2.center_rel.x, cell2.center_rel.y);
+
+        let line_len = rect2.x() - rect1.x();
+
+        this._group.polyline([
+            [0, 0],
+            [line_len, 0]
+        ])
+            .stroke({width: 1})
+            .fill('none')
+            .move(rect1.cx(), rect1.cy());
+
+        let center_x = cell1.center_rel.x + (cell2.pos.x - cell1.pos.x) / 2;
+        let center_y = cell1.center_rel.y;
+
+        this._group.circle(cell1.size.y/1.25)
+            .center(
+                center_x,
+                center_y
+            )
+            .fill('#ffffff')
+            .stroke({width: 1});
+
+        this._group.text('M')
+            .font({
+                size: 22,
+                weight: 'bold',
+                family: "monospace",
+                anchor: "start"
+            })
+            .center(center_x, center_y - 5);
+
+        this._group.line(center_x - 10, center_y + 7, center_x + 10, center_y + 7)
+            .stroke({width: 2})
+            .fill('none');
+
+        this._group.line(center_x - 10, center_y + 12, center_x - 2, center_y + 12)
+            .stroke({width: 2})
+            .fill('none');
+
+        this._group.line(center_x + 2, center_y + 12, center_x + 10, center_y + 12)
+            .stroke({width: 2})
+            .fill('none');
     }
 
     _drawLabel(text="", size=Plate.LabelSizeDefault) {
