41677f791c3f15331349d1e16af8b58578eccb19
overklok-pc <john_overklok@yahoo.com>
2020-05-24 22:10:34 +0500

    docs

--- app/js/core/layout/components/Frame.tsx
+++ app/js/core/layout/components/Frame.tsx
@@ -1,13 +1,22 @@
 import * as React from "react";
+import {useDrop} from "react-dnd";
+import {DraggableItemTypes} from "../types";
 
 interface IProps {
-    children: JSX.Element // usually a Nest creators
+    children: JSX.Element|JSX.Element[]
 }
 
 export default function Frame(props: IProps) {
+    const [{is_over}, drop] = useDrop({
+        accept: DraggableItemTypes.Tab,
+        drop: () => {},
+        collect: monitor => ({
+            is_over: !!monitor.isOver(),
+        }),
+    });
 
     return (
-        <div className='frame'>
+        <div className='frame' ref={drop}>
             {props.children}
         </div>
     )
