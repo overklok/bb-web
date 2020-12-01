4a3b465774da0d3779685fb63eb975978e44dca0
overklok-pc <john_overklok@yahoo.com>
2020-12-02 01:15:19 +0500

    Nest options; WIP: React Navbar

--- app/js/views/controls/TopbarView.tsx
+++ app/js/views/controls/TopbarView.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 import {ViewEvent} from "../../core/base/Event";
-import {IViewProps, IViewState, View} from "../../core/base/view/View";
+import {AllProps, IViewProps, IViewState, View} from "../../core/base/view/View";
 
 require('../../../css/blocks/menu/navbar.less')
 require('../../../css/blocks/menu/progressbar.less')
@@ -30,6 +30,26 @@ export namespace TopbarView {
     }
 
     export class TopbarView extends View<Props, State> {
+        constructor(props: AllProps<Props>) {
+            super(props);
+
+            this.onScrollableUpdate = this.onScrollableUpdate.bind(this);
+        }
+
+        private onScrollableUpdate(ref: HTMLElement) {
+            if (!ref) return;
+
+            ref.addEventListener('wheel', function(e) {
+                if (e.deltaY > 0) {
+                    ref.scrollTo(ref.scrollLeft + 10, 0);
+                }
+
+                if (e.deltaY < 0) {
+                    ref.scrollTo(ref.scrollLeft + 10, 0);
+                }
+            });
+        }
+
         public render(): React.ReactNode {
             return (
                 <React.Fragment>
@@ -48,24 +68,26 @@ export namespace TopbarView {
                         <div className="navbar__section navbar__section_half">
                             <div className="pager">
                                 <div className="pager__arrow pager__arrow_left"/>
-                                <ul className="pager__list">
-                                    <li className="pager__item cask">1</li>
-                                    <li className="pager__item cask">2</li>
-                                    <li className="pager__item cask">3</li>
-                                    <li className="pager__item cask">4</li>
-                                    <li className="pager__item cask">5</li>
-                                    <li className="pager__item cask">6</li>
-                                    <li className="pager__item cask">7</li>
-                                    <li className="pager__item cask">8</li>
-                                    <li className="pager__item cask">9</li>
-                                    <li className="pager__item cask">A</li>
-                                    <li className="pager__item cask">B</li>
-                                    <li className="pager__item cask">C</li>
-                                    <li className="pager__item cask">D</li>
-                                    <li className="pager__item cask">E</li>
-                                    <li className="pager__item cask">F</li>
-                                    <li className="pager__item cask">G</li>
-                                </ul>
+                                <div className="pager__listwrap">
+                                    <ul className="pager__list" ref={this.onScrollableUpdate}>
+                                        <li className="pager__item cask">1</li>
+                                        <li className="pager__item cask">2</li>
+                                        <li className="pager__item cask">3</li>
+                                        <li className="pager__item cask">4</li>
+                                        <li className="pager__item cask">5</li>
+                                        <li className="pager__item cask">6</li>
+                                        <li className="pager__item cask">7</li>
+                                        <li className="pager__item cask">8</li>
+                                        <li className="pager__item cask">9</li>
+                                        <li className="pager__item cask">A</li>
+                                        <li className="pager__item cask">B</li>
+                                        <li className="pager__item cask">C</li>
+                                        <li className="pager__item cask">D</li>
+                                        <li className="pager__item cask">E</li>
+                                        <li className="pager__item cask">F</li>
+                                        <li className="pager__item cask">G</li>
+                                    </ul>
+                                </div>
                                 <div className="pager__arrow pager__arrow_right"/>
                             </div>
                         </div>
