0634a5a57596534f937edc46c429a74c13e26565
overklok-mjl <john_overklok@yahoo.com>
2021-01-09 23:17:05 +0500

    renamed current config to 'old' and 'new' to current

--- webpack.config.js
+++ webpack.config.js
@@ -11,107 +11,111 @@ const webpack = require('webpack');
 const dotenv = require('dotenv').config();
 const { generate } = require('build-number-generator');
 
-const Dotenv                = require('dotenv-webpack');
-const HtmlWebpackPlugin     = require('html-webpack-plugin');
-const CopyWebpackPlugin     = require('copy-webpack-plugin');
-// const BundleAnalyzerPlugin  = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const BuildNotifierPlugin   = require('webpack-build-notifier');
-
 const lib_dir = __dirname + '/vendor/js';
 
+const TerserPlugin                      = require('terser-webpack-plugin');
+const HtmlWebpackPlugin                 = require('html-webpack-plugin');
+const CopyWebpackPlugin                 = require('copy-webpack-plugin');
+const BuildNotifierPlugin               = require('webpack-build-notifier');
+const BundleAnalyzerPlugin              = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const MiniCssExtractPlugin              = require('mini-css-extract-plugin');
+const LodashModuleReplacementPlugin     = require('lodash-webpack-plugin');
+
+
 module.exports = (env, argv) => {
-    const is_dev = env.mode === "development";
+    resolveEnvEntries(env);
+
+    const no_copy = !!argv.liveReload;
+    const is_dev = argv.mode === "development";
     const [ver, tgt, mode] = [getVersionNumber(), getVersionTarget(env), getVersionMode(argv.mode)]
-    const VERSION = mode ? `${tgt}/${ver}-${mode}` : `${tgt}/${ver}`;
-    
+    const VERSION = mode ? `${tgt}/${ver}-${mode} [new]` : `${tgt}/${ver} [new]`;
+
     console.info("Building", VERSION);
 
     return {
         entry: getEntries(env),
-        devtool: is_dev ? 'eval-source-map' : 'source-map', // 'source-map' for production
+        devtool: is_dev ? 'eval-source-map' : 'eval-cheap-source-map',
+        optimization: {
+            minimizer: getMinimizer(is_dev)
+        },
         output: {
-            filename: '[name].js',
-            path: path.resolve(__dirname, 'dist')
+            publicPath: "/"
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            historyApiFallback: {
+                index: '/',
+            },
+            index: getHtmlIndexFile(env),
+            compress: true,
+            port: 9000
         },
         module: {
             rules: [
-                // {
-                //     loader: 'jshint-loader',
-                //     test: /\.js$/,  // запустить загрузчик во всех файлах .js
-                //     exclude: /node_modules/ // игнорировать все файлы в папке node_modules
-                // },
                 {
-                    loader: 'babel-loader',
-                    test: /\.js$/,
-                    exclude: /node_modules/
+                    loader: 'ts-loader',
+                    test: /\.(tsx?|jsx?)$/,
+                    exclude: /node_modules/,
                 },
                 {
-                    use: 'ts-loader',
-                    test: /\.ts?$/,
-                    exclude: /node_modules/
-                },
-                {
-                    loaders: ['style-loader', 'css-loader'],
-                    test: /\.css/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
+                    test: /\.(less|css)/,
                     include: [
-                        path.resolve(__dirname, "node_modules/intro.js/"),
                         path.resolve(__dirname, "app"),
-                    ]
+                    ],
                 },
-                {
-                    test: require.resolve('jquery'),
-                    use: [
-                        {
-                            loader: 'expose-loader',
-                            options: '$'
-                        },
-                    ]
-                }
             ]
         },
         resolve: {
-            extensions: [ '.tsx', '.ts', '.js' ]
+            extensions: ['.tsx', '.ts', '.js'],
+            modules: [path.resolve(__dirname, './app'), 'node_modules']
         },
-        externals: [
-            (function () {
-                let IGNORES = [
-                    'electron'
-                ];
-                return function (context, request, callback) {
-                    if (IGNORES.indexOf(request) >= 0) {
-                        return callback(null, "(typeof require === \"function\") ? require('" + request + "') : undefined");
-                    }
-                    return callback();
-                };
-            })()
-        ],
         plugins: [
-            new Dotenv(),
+            new MiniCssExtractPlugin(),
+            new LodashModuleReplacementPlugin(),
             ...getHtmlCopyPluginInstances(env),
-            new CopyWebpackPlugin(getCopypaths(env)),
+            new BuildNotifierPlugin({
+                title: "Tapanda [New]",
+                logo: path.resolve("./img/favicon.png"),
+            }),
+            new CopyWebpackPlugin(getCopypaths(env, is_dev, no_copy)),
             new webpack.DefinePlugin({
-                // 'process.env.NODE_ENV': '"production"',
                 '__VERSION__': `'${VERSION}'`,
             }),
-            new webpack.ProvidePlugin({
-                $: 'jquery',
-                // jQuery: 'jquery'
-            }),
-            new webpack.WatchIgnorePlugin([
-                /\.d\.ts$/
-            ]),
-            new BuildNotifierPlugin({
-              title: "My Project Webpack Build",
-              logo: path.resolve("./img/favicon.png"),
-            }),
-            // new BundleAnalyzerPlugin({
-            //     analyzerMode: 'static',
-            //     openAnalyzer: true,
-            //     generateStatsFile: true,
-            //     statsOptions: {source: false}
-            // }),
-        ],
-    };
+            // new BundleAnalyzerPlugin()
+        ]
+    }
+};
+
+function getMinimizer(is_dev) {
+    if (is_dev) return [];
+
+    return [new TerserPlugin({
+        cache: !is_dev,
+        parallel: true,
+        sourceMap: is_dev,
+    })];
+}
+
+function resolveEnvEntries(env) {
+    if (env.all) {
+        console.warn("WARNING: 'main' entry has been temporarily excluded for mixed build");
+        // env.main = true;
+        env.board = true;
+        env.monkey = true;
+        env.playground = true;
+    }
+}
+
+function getEntries(env) {
+    // Bundle entries
+    let entries = {};
+    if (env.main === true)          entries['main']         = './app/js/MainApplication.ts';
+    if (env.board === true)         entries['board']        = './app/js/BoardApplication.ts';
+    if (env.monkey === true)        entries['monkey']       = './app/js/MonkeyApplication.ts';
+    if (env.playground === true)    entries['playground']   = './app/js/PlaygroundApplication.ts';
+
+    return entries;
 }
 
 function getVersionNumber() {
@@ -123,69 +127,15 @@ function getVersionMode(mode) {
 }
 
 function getVersionTarget(env, mode=null) {
-    let matches = [env.main, env.board, env.blockly]
+    let matches = [env.main, env.board, env.monkey, env.playground]
         .reduce((total, v_curr) => total += (v_curr === true), 0);
 
     if (matches > 1) return `mixed`;
 
-    if (env.main === true)      return `main`;
-    if (env.board === true)     return `board`;
-    if (env.blockly === true)   return `blockly`;
-}
-
-function getEntries(env) {
-    // Bundle entries
-    let entries = {};
-    if (env.main === true)      entries['main'] = './app/js/index.js';
-    if (env.board === true)     entries['board'] = './app/js/admin_board.js';
-    if (env.blockly === true)   entries['blockly'] = './app/js/admin_blockly.js';
-
-    return entries;
-}
-
-function getCopypaths(env) {
-    if (!dotenv.parsed) {
-        console.warn("Nothing to copy.");
-        return [];
-    }
-
-    // Copy paths
-    let copypaths = [];
-
-    if (env.main === true && dotenv.parsed.PATH_DIST_MAIN) {
-        copypaths = [...copypaths,
-            {from: './app/fonts/',          to: './fonts'},
-            {from: './app/fonts/',          to: dotenv.parsed.PATH_DIST_MAIN + '/fonts'},
-            {from: './app/css/spinner.css', to: './spinner.css'},
-            {from: './app/css/spinner.css', to: dotenv.parsed.PATH_DIST_MAIN + '/css/spinner.css'},
-            {from: './app/images/',         to: dotenv.parsed.PATH_DIST_MAIN + '/images'},
-            {from: './dist/main.js',      to: dotenv.parsed.PATH_DIST_MAIN + '/js/main.js'},
-        ];
-    }
-
-    if (env.board === true && dotenv.parsed.PATH_DIST_BOARD_ADMIN) {
-        copypaths = [...copypaths,
-            {from: './dist/board.js',     to: dotenv.parsed.PATH_DIST_BOARD_ADMIN + '/admin_board.js'}
-        ];
-    }
-    if (env.board === true && dotenv.parsed.PATH_DIST_BOARD) {
-        copypaths = [...copypaths,
-            {from: './dist/board.js',     to: dotenv.parsed.PATH_DIST_BOARD + '/board.js'}
-        ];
-    }
-    if (env.board === true && dotenv.parsed.PATH_DIST_BOARD_SOCK) {
-        copypaths = [...copypaths,
-            {from: './dist/board.js',     to: dotenv.parsed.PATH_DIST_BOARD_SOCK + '/board.js'}
-        ];
-    }
-
-    if (env.blockly === true && dotenv.parsed.PATH_DIST_BLOCKLY) {
-        copypaths = [...copypaths,
-            {from: './dist/blockly.js',   to: dotenv.parsed.PATH_DIST_BLOCKLY + '/admin_blockly.js'},
-        ];
-    }
-
-    return copypaths;
+    if (env.main === true)          return `main`;
+    if (env.board === true)         return `board`;
+    if (env.monkey === true)        return `monkey`;
+    if (env.playground === true)    return `playground`;
 }
 
 function getHtmlCopyPluginInstances(env) {
@@ -194,8 +144,9 @@ function getHtmlCopyPluginInstances(env) {
     if (env.main === true) {
         htmls = [...htmls,
             new HtmlWebpackPlugin({
-                template: './app/html/index.html',
-                inject: 'body'
+                template: './app/html/main.html',
+                inject: 'body',
+                filename: 'main.html'
             }),
         ];
     }
@@ -208,15 +159,90 @@ function getHtmlCopyPluginInstances(env) {
             }),
         ];
     }
-    if (env.blockly === true) {
+    if (env.monkey === true) {
         htmls = [...htmls,
             new HtmlWebpackPlugin({
-                template: './app/html/admin.html',
+                template: './app/html/monkey.html',
                 inject: 'body',
-                filename: 'admin.html'
+                filename: 'monkey.html'
+            }),
+        ];
+    }
+    if (env.playground === true) {
+        htmls = [...htmls,
+            new HtmlWebpackPlugin({
+                template: './app/html/playground.html',
+                inject: 'body',
+                filename: 'playground.html'
             }),
         ];
     }
 
     return htmls;
+}
+
+function getHtmlIndexFile(env) {
+    if (env.main === true) return 'main.html';
+    if (env.board === true) return 'board.html';
+    if (env.monkey === true) return 'monkey.html';
+    if (env.playground === true) return 'playground.html';
+}
+
+function getCopypaths(env, is_dev, no_copy) {
+    if (!dotenv.parsed || no_copy) {
+        console.warn("Nothing to copy.");
+        return [];
+    }
+
+    const settings = [
+        {
+            enable: env.board === true,
+            paths: [
+                dotenv.parsed.PATH_DIST_BOARD,
+                dotenv.parsed.PATH_DIST_BOARD_SOCK,
+                dotenv.parsed.PATH_DIST_BOARD_ADMIN
+            ],
+            entry: 'board'
+        },
+        {
+            enable: env.monkey === true,
+            paths: [dotenv.parsed.PATH_DIST_MONKEY],
+            entry: 'monkey'
+        },
+        {
+            enable: env.playground === true,
+            paths: [dotenv.parsed.PATH_DIST_PLAYGROUND],
+            entry: 'playground'
+        }
+
+    ]
+
+    console.log("Copy settings", settings);
+
+    // Copy paths
+    let copypaths = [];
+
+    for (const setting of settings) {
+        for (const path of setting.paths) {
+            if (setting.enable && path) {
+                copypaths = [...copypaths,
+                    {
+                        from: `./dist/${setting.entry}.js`,
+                        to: path + `/${setting.entry}.js`
+                    },
+                ];
+
+                if (is_dev) {
+                    copypaths = [...copypaths,
+                        {
+                            from: `./dist/${setting.entry}.js.map`,
+                            to: path + `/${setting.entry}.js.map`
+                        },
+                    ];
+                }
+            }
+        }
+    }
+
+    return copypaths;
 }
